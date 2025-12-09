25f24579d6cc747c176bee228fdc1753cb61de3d
Shamimm-Faqer <219502059+Shamim-Faqer@users.noreply.github.com>
2025-12-10 02:54:00 +0600

    engJanalaFunction

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+alert("eidai😅")
 const loadLevel = () => {
   fetch('https://openapi.programming-hero.com/api/levels/all')
     .then(res => res.json())
@@ -10,6 +11,7 @@ const loadLevel = () => {
 const displayLevel = (lesson) => {
   // 1. Get the Container
   const levelContainer = document.getElementById("levelContainer");
+  levelContainer.innerHTML="";
   
   // 2. Get every level
   lesson.forEach((lesson) => {
@@ -33,18 +35,26 @@ const displayWord = (id) => {
 
 const displayWords = (words) => {
   const displayWordContainer = document.getElementById("displayWordContainer");
-  displayWordContainer.innerHTML = "";
+  displayWordContainer.innerHTML="";
+  if (words.length == 0) {
+    displayWordContainer.innerHTML = `<div class=" grid lg:col-span-full md:col-span-2 items-center justify-center max-w-full  bg-base-300 rounded shadow-lg p-4">
+      <div class="flex flex-1 justify-center items-center"><img  src="assets/alert-error.png" alt=""> </div>
+   <p class="text-center bangla-text">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+   <h2 class="bangla-text text-center">নেক্সট Lesson এ যান</h2>
+ </div>`;
+  }
+  
   words.forEach((words)=>{
     const wordsDiv = document.createElement ("div");
-    wordsDiv.innerHTML = `<div class=" flex-col max-w-fit bg-primary-content shadow-lg p-4 text-primary ">
+    wordsDiv.innerHTML = `<div class=" flex-col max-w-fit bg-base-300 border-white rounded shadow-lg p-4 ">
    <p class="">Level - ${words.level}</p>
    <p class="">Word - ${words.word}</p>
    <p class="">Meaning - ${words.meaning}</p>
    <p class="">Pronounciation - ${words.pronunciation}</p>
  </div>`;
     displayWordContainer.append(wordsDiv);
-  } )
-}
+  } );
+};
 
 loadLevel();
 
